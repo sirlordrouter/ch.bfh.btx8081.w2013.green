f6f0418bb5354e4f2d64b0cc2f6a16bb3b38afb3
Johannes Gnägi <johannes3004@gmail.com>
2013-11-25 14:51:53 +0100

    Changed gitignore and experimented with vaadin scrum first sprint defined

--- src/main/java/ch/bfh/btx8081/w2013/green/MyVaadinUI.java
+++ src/main/java/ch/bfh/btx8081/w2013/green/MyVaadinUI.java
@@ -4,11 +4,14 @@ import javax.servlet.annotation.WebServlet;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -18,23 +21,52 @@ public class MyVaadinUI extends UI
 {
 
     @WebServlet(value = "/*", asyncSupported = true)
-    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "ch.bfh.btx8081.w2013.green.AppWidgetSet")
+    @VaadinServletConfiguration(productionMode = false, 
+    	ui = MyVaadinUI.class, widgetset = "ch.bfh.btx8081.w2013.green.AppWidgetSet")
     public static class Servlet extends VaadinServlet {
     }
 
     @Override
     protected void init(VaadinRequest request) {
+    	
+
         final VerticalLayout layout = new VerticalLayout();
         layout.setMargin(true);
         setContent(layout);
         
-        Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
+        
+        Button btnHelp = new Button("HELP");
+        btnHelp.addClickListener(new Button.ClickListener() {
             public void buttonClick(ClickEvent event) {
                 layout.addComponent(new Label("Thank you for clicking"));
             }
         });
-        layout.addComponent(button);
+        
+        Button btnSkills = new Button("SKILLS");
+        btnSkills.addClickListener(new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                layout.addComponent(new Label("Thank you for clicking"));
+            }
+        });
+        
+        Button btnMedication = new Button("MEDICATION");
+        btnMedication.addClickListener(new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                Notification.show("You shoud now take your Medication XY");
+            }
+        });
+        
+        Button btnSettings = new Button("SETTINGS");
+        btnSettings.addClickListener(new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                layout.addComponent(new Label("Thank you for clicking"));
+            }
+        });
+        
+        layout.addComponent(btnHelp);
+        layout.addComponent(btnSkills);
+        layout.addComponent(btnMedication);
+        layout.addComponent(btnSettings);
     }
 
 }
