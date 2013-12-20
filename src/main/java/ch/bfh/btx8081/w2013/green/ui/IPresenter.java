4fe1b1c5b6921007a0b162b90b3a8465a88f5c91
Johannes Gnägi <johannes3004@gmail.com>
2013-12-20 13:18:57 +0100

    StartSettingsView with mvp

---
+++ src/main/java/ch/bfh/btx8081/w2013/green/ui/IPresenter.java
@@ -0,0 +1,21 @@
+package ch.bfh.btx8081.w2013.green.ui;
+
+/**
+ * Berner Fachhochschule</br>
+ * Medizininformatik BSc</br>
+ * Modul 8081, HS2013</br>
+ *
+ *<p>Class Description</p>
+ *
+ * @author Johannes Gnaegi, gnaegj1@bfh.ch
+ * @version 20-12-2013
+ */
+public interface IPresenter {
+
+    void addPresenter(IPresenter p);
+
+    public interface IPresenter {
+
+        void btnBackClicked();
+    }
+}
---
+++ src/main/java/ch/bfh/btx8081/w2013/green/ui/start/StartSettingsPresenter.java
@@ -0,0 +1,7 @@
+package ch.bfh.btx8081.w2013.green.ui.start;
+
+/**
+ * Created by Johannes on 20.12.13.
+ */
+public class StartSettingsPresenter {
+}
